6939c79a64f213d321228388298de0d3b16ac174
Chai Chai <44784077+ChaiChai676@users.noreply.github.com>
2024-01-25 17:50:33 +0000

    Formatting revamp

--- scripts/main.js
+++ scripts/main.js
@@ -7,195 +7,23 @@ const codeContents = {
 `,
 'nr-2-1':
 `
-UENUM(BlueprintType)
-enum class EGearCategory : uint8
-{
-    None,
-    Backpack,
-    Costume,
-    Flashlight,
-    Footwear,
-    Vision,
-    Miscellaneous,
-};
-
-UENUM(BlueprintType)
-enum class EDegradationType : uint8
-{
-    None,
-    Durability,
-    Charge,
-};
-
-USTRUCT(BlueprintType)
-struct FGearData : public FTableRowBase
-{
-    GENERATED_BODY()
-    
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
-    FString Name;   
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
-    EGearCategory GearCategory;
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
-    FString Description;    
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
-    FName BoneName;
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
-    UStaticMesh* Mesh;
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
-    float Cost;
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
-    UTexture2D* ItemImage;
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
-    EDegradationType DegradationType;
-    UPROPERTY(EditAnywhere, BlueprintReadOnly)
-    int DegradationValue;
-};`,
+`,
 'nr-2-2':
 `
 `,
 'qnn-1-1':
 `
-  void AGrapplingArm::InputBind(AQNNPlayerCharacter* Character)
-  {
-    if (Character)
-    {
-      if (AQNNPlayerController* PlayerController = Cast<AQNNPlayerController>(Character->GetController()))
-      {
-        if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
-        {
-          Subsystem->AddMappingContext(GrapplingContext, 0);
-          if (UQNNEnhancedInputComponent* QNNEnhancedInputComponent = Cast<UQNNEnhancedInputComponent>(Character->InputComponent))
-          {
-            const FQNNGameplayTags& GameplayTags = FQNNGameplayTags::Get();
-            
-            QNNEnhancedInputComponent->BindActionByTag(PlayerController->InputConfig, GameplayTags.InputTag_Grappling_Launch, ETriggerEvent::Started, this, &AGrapplingArm::LaunchGrapple);
-            QNNEnhancedInputComponent->BindActionByTag(PlayerController->InputConfig, GameplayTags.InputTag_Grappling_Cancel, ETriggerEvent::Started, this, &AGrapplingArm::CancelGrapple);
-          }
-        }
-      }
-    }
-  }
-}`,
+`,
 'qnn-1-2':
 `
-void AGrapplingArm::InputUnbind(AQNNPlayerCharacter* Character)
-{
-    if (Character)
-    {
-        if (APlayerController* PlayerController = Cast<APlayerController>(Character->GetController()))
-        {
-            if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
-            {
-                Subsystem->RemoveMappingContext(GrapplingContext);
-                if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(Character->InputComponent))
-                {
-                    EnhancedInputComponent->ClearBindingsForObject(this);
-                }
-            }
-        }
-    }
-}`,
+`,
 'qnn-2':
 `
 
 `,
 'qnn-3':
 `
-  void UQNNMovementComponent::BeginPlay()
-  {
-    Super::BeginPlay();
-
-    Duration = FMath::Clamp(Duration, 0.2f, 9000.0f);
-    if (GetOwner())
-    {
-      StartLocation = GetOwner()->GetActorLocation();
-      StartRotation = GetOwner()->GetActorRotation();
-    }
-
-    // When bLoop = false, prevents movement on BeginPlay
-    if (Switch)
-    {
-      bMCActivated = Switch->bSwitchActivated;
-    }
-  }
-
-  void UQNNMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-  {
-    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-    if (Switch)
-    {
-      if (bMCActivated != Switch->bSwitchActivated)
-      {
-        bSwitchToggled = true;
-      }
-      bMCActivated = Switch->bSwitchActivated;
-    }
-    
-    if (bLoop)
-    {
-      if (bMCActivated)
-      {
-        Move(DeltaTime);
-
-        if (TimePassed > Duration)
-        {
-          TimePassed = 0;
-          if (bForward)
-          {
-            bForward = false;
-          }
-          else
-          {
-            bForward = true;
-          }
-        }
-      }
-    } else
-    {
-      if (bSwitchToggled)
-      {
-        Move(DeltaTime);
-        
-        if (TimePassed > Duration)
-        {
-          TimePassed = 0;
-          if (bForward)
-          {
-            bForward = false;
-          }
-          else
-          {
-            bForward = true;
-          }
-          bSwitchToggled = false;
-        }
-      }
-    }
-  }
-
-  void UQNNMovementComponent::Move(float DeltaTime)
-  {
-    if (GetOwner())
-    {
-      TimePassed += DeltaTime;
-      const float TimeRatio = FMath::Clamp(TimePassed/Duration, 0.0f, 1.0f);
-      if (bForward)
-      {
-        CurrentLocation = FMath::Lerp(StartLocation, StartLocation+RelativeLocation, TimeRatio);
-        CurrentRotation = FMath::Lerp(StartRotation, StartRotation+RelativeRotation, TimeRatio);
-      }
-      else
-      {
-        CurrentLocation = FMath::Lerp(StartLocation+RelativeLocation, StartLocation, TimeRatio);
-        CurrentRotation = FMath::Lerp(StartRotation+RelativeRotation, StartRotation, TimeRatio);
-      }
-      
-      GetOwner()->SetActorLocation(CurrentLocation);
-      GetOwner()->SetActorRotation(CurrentRotation);
-    }
-  }
+code code code
 `,
 'qnn-4':
 `
